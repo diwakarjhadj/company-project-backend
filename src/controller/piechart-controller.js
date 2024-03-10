adfa6e7c5dc5a14fdf29dfc7c34bdb4954c66211
diwakarjhadj <dkj9910419457@gmail.com>
2024-03-10 17:50:03 +0530

    pie-chart completed

---
+++ src/controller/piechart-controller.js
@@ -0,0 +1,37 @@
+const Transaction = require('../model/seeds.js');
+const { getMonthQuery } = require('../../utils/monthUtils.js');
+const piechart = async (req, res) => {
+    try {
+        const { month } = req.query;
+
+        // Retrieve transactions for the selected month
+        const monthQuery = getMonthQuery(month);
+        const transactions = await Transaction.find({
+            dateOfSale: monthQuery // Match any day in the selected month
+        });
+        console.log(transactions);
+
+        // Extract unique categories from the transactions
+        const uniqueCategories = [...new Set(transactions.map(transaction => transaction.category))];
+
+        // Count the number of items for each unique category
+        const categoryCounts = {};
+        uniqueCategories.forEach(category => {
+            categoryCounts[category] = transactions.filter(transaction => transaction.category === category).length;
+        });
+
+        // Prepare the response data
+        const responseData = {};
+        uniqueCategories.forEach(category => {
+            responseData[category] = categoryCounts[category];
+        });
+
+        // Send the response
+        res.json(responseData);
+    } catch (error) {
+        console.error('Error fetching pie chart data:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+module.exports = piechart;
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const getInitialdata = require('../controller/seeds-controller.js');
 const transactionController = require('../controller/transaction-controller.js');
 const statisticsController = require('../controller/statistics-controller.js');
 const barchartController = require('../controller/barchart-controller.js');
-
+const piechartController = require('../controller/piechart-controller.js');
 const route = express.Router();
 
 route.use(express.json());
@@ -12,7 +12,7 @@ route.get('/initialize-database', getInitialdata);
 route.get('/transactions', transactionController);
 route.get('/statistics', statisticsController);
 route.get('/bar-chart', barchartController);
-// route.post('/conversation/get',getConversation);
+route.get('/pie-chart',piechartController);
 // route.post('/message/add',newMessage);
 // route.get('/message/get/:id',getMessages);
 // route.post('/file/upload',upload.single("file"),uploadFile);
